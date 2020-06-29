7c40cf5d550b7d7db72e8f7ddc0d724baf381ec6
tommylees112 <thomas.lees112@gmail.com>
2020-06-29 17:51:18 +0100

    catch validation errors

--- codebase/evaluation/tester.py
+++ codebase/evaluation/tester.py
@@ -210,38 +210,46 @@ class Tester(object):
             else:
                 start_date = self.cfg[f"{self.mode}_start_date"]
 
-            # determine the end of the first sequence (first target in sequence-to-one)
-            date_range = pd.date_range(
-                start=start_date, end=self.cfg[f"{self.mode}_end_date"]
-            )
+            # TODO: why are there less dates than the true number of dates?
+            # There must be missing data for those timesteps
 
-            xr = xarray.Dataset(
-                data_vars=data,
-                coords={
-                    "date": date_range,
-                    "time_step": np.arange(-self.cfg["predict_last_n"] + 1, 1),
-                },
-            )
-            results[basin]["xr"] = xr
+            try:
+                # determine the end of the first sequence (first target in sequence-to-one)
+                date_range = pd.date_range(
+                    start=start_date, end=self.cfg[f"{self.mode}_end_date"]
+                )
 
-            if metrics:
-                qobs_variable_name = [
-                    col for col in self.cfg["target_variable"] if "qobs" in col.lower()
-                ]
-                if qobs_variable_name:
-                    qobs_variable_name = qobs_variable_name[0]
+                xr = xarray.Dataset(
+                    data_vars=data,
+                    coords={
+                        "date": date_range,
+                        "time_step": np.arange(-self.cfg["predict_last_n"] + 1, 1),
+                    },
+                )
+                results[basin]["xr"] = xr
 
-                    # check if not empty (in case no streamflow data exist in validation period
-                    qobs = xr[f"{qobs_variable_name}_obs"].sel(dict(time_step=0))
-                    qsim = xr[f"{qobs_variable_name}_sim"].sel(dict(time_step=0))
-                    if (len(qsim.shape) > 1) and (qsim.shape[-1] > 0):
-                        # print("Using the mean discharge for metric calculation")
-                        qsim = qsim.mean(axis=-1)
-                    values = calculate_metrics(qobs, qsim, metrics=metrics)
-                    if logger is not None:
-                        logger.log_step(**values)
-                    for k, v in values.items():
-                        results[basin][k] = v
+                if metrics:
+                    qobs_variable_name = [
+                        col for col in self.cfg["target_variable"] if "qobs" in col.lower()
+                    ]
+                    if qobs_variable_name:
+                        qobs_variable_name = qobs_variable_name[0]
+
+                        # check if not empty (in case no streamflow data exist in validation period
+                        qobs = xr[f"{qobs_variable_name}_obs"].sel(dict(time_step=0))
+                        qsim = xr[f"{qobs_variable_name}_sim"].sel(dict(time_step=0))
+                        if (len(qsim.shape) > 1) and (qsim.shape[-1] > 0):
+                            # print("Using the mean discharge for metric calculation")
+                            qsim = qsim.mean(axis=-1)
+                        values = calculate_metrics(qobs, qsim, metrics=metrics)
+                        if logger is not None:
+                            logger.log_step(**values)
+                        for k, v in values.items():
+                            results[basin][k] = v
+
+            except ValueError as e:
+                print(f"** Basin {basin} Has the wrong number of timesteps. Exp: {len(date_range)} Got: {len(y_hat)}**")
+                basins_without_train_data.append(basin)
 
         if (self.mode == "validation") and (self.cfg.get("log_n_figures", 0) > 0):
             self._create_and_log_figures(results, logger, epoch)
