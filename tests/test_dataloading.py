bf09e833c3b1bca23d3140e229013185e7ef9f54
tommylees112 <thomas.lees112@gmail.com>
2020-07-03 15:19:12 +0100

    update tests

--- tests/test_dataloading.py
+++ tests/test_dataloading.py
@@ -20,10 +20,14 @@ class TestCmdLine:
         # check models saved / checkpointed
         assert all(np.isin(["model_epoch001.pt", "model_epoch002.pt"], [d.name for d in run_dir.iterdir()]))
         # check features created
-        assert all(np.isin(["train_data.h5", "train_data_scaler.p"], [d.name for d in run_dir.iterdir()]))
+        assert all(np.isin(["train_data.h5", "train_data_scaler.p"], [d.name for d in (run_dir / "train_data").iterdir()]))
 
         # run evaluation command
         eval_cmd = f"ipython main.py evaluate -- --run_dir {run_dir}"
         print(f"Running evaluation:\n\t{eval_cmd}")
         os.system(eval_cmd)
-        assert False
+        assert (run_dir / "test/model_epoch002").exists(), "Expect 2nd Epoch to have produced test directory"
+        assert (run_dir / "test/model_epoch002/test_results.p").exists(), "Expect 2nd Epoch to have produced test results"
+
+        # rm test directory
+        # os.system(f"rm -rf {run_dir}")
