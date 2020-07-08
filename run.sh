9a642b8fb14ac6f085df7d0d083101de80fc2768
tommylees112 <thomas.lees112@gmail.com>
2020-07-08 22:03:35 +0100

    update all

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
-# ---- -----
-ipython main.py train -- --config_dir configs/aws_lstm.yml
-ipython main.py train -- --config_dir configs/aws_ealstm.yml
+# ---- run the training -----
+ipython main.py train -- --config_file configs/aws_lstm.yml
+ipython main.py train -- --config_file configs/aws_ealstm.yml
 
-dir='/home/tommy/tommy_multiple_forcing/runs/'
+dir='/home/tommy/tommy_multiple_forcing/runs'
 
-# ---- run the evaluation -----
+# ---- get the runs -----
 unset -v lstm_run
-for file in "$dir"/tomm*; do
+for file in "$dir"/lstm*; do
   [[ $file -nt $lstm_run ]] && lstm_run=$file
 done
 
@@ -15,5 +15,10 @@ for file in "$dir"/lstm*; do
   [[ $file -nt $ealstm_run ]] && ealstm_run=$file
 done
 
+# ---- run the evaluation -----
+ipython main.py evaluate -- --run_dir $lstm_run
+ipython main.py evaluate -- --run_dir $ealstm_run
+
 # ---- run the analysis -----
-ipython analysis/datautils.py -- --run_dir runs/tommy_camels_gb_0607_1221/
+ipython analysis/datautils.py -- --run_dir $lstm_run
+ipython analysis/datautils.py -- --run_dir $ealstm_run
