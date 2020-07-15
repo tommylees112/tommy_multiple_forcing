13e3eaa657482c66c22412fd449c092d4ddfd158
tommylees112 <thomas.lees112@gmail.com>
2020-07-15 11:24:47 +0100

    update run script

--- run.sh
+++ run.sh
@@ -1,24 +1,29 @@
-# ---- run the training -----
-ipython main.py train -- --config_file configs/aws_lstm.yml
-ipython main.py train -- --config_file configs/aws_ealstm.yml
-
 dir='/home/tommy/tommy_multiple_forcing/runs'
 
-# ---- get the runs -----
+# ---- run the LSTM -----
+ipython main.py train -- --config_file configs/aws_lstm.yml
+
 unset -v lstm_run
 for file in "$dir"/lstm*; do
   [[ $file -nt $lstm_run ]] && lstm_run=$file
 done
+
+ipython analysis/analyse_all_epochs.py -- --run_dir $lstm_run
+
+# ---- run the EALSTM -----
+ipython main.py train -- --config_file configs/aws_ealstm.yml
 
 unset -v ealstm_run
 for file in "$dir"/ealstm*; do
   [[ $file -nt $ealstm_run ]] && ealstm_run=$file
 done
 
-# ---- run the evaluation -----
-ipython main.py evaluate -- --run_dir $lstm_run
-ipython main.py evaluate -- --run_dir $ealstm_run
+ipython analysis/analyse_all_epochs.py -- --run_dir $ealstm_run
 
-# ---- run the analysis -----
-ipython analysis/datautils.py -- --run_dir $lstm_run
-ipython analysis/datautils.py -- --run_dir $ealstm_run
+# # ---- run the evaluation -----
+# ipython main.py evaluate -- --run_dir $lstm_run
+# ipython main.py evaluate -- --run_dir $ealstm_run
+
+# # ---- run the analysis -----
+# ipython analysis/datautils.py -- --run_dir $lstm_run
+# ipython analysis/datautils.py -- --run_dir $ealstm_run
